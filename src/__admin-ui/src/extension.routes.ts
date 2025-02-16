83cf4af7fee850ea6829e9557b5ba0a3a929c44b
Aurélien <2128970+GoldAndLink@users.noreply.github.com>
2025-02-16 20:44:55 -0300

    fix: custom plugin + admin ui plugins

--- src/__admin-ui/src/extension.routes.ts
+++ src/__admin-ui/src/extension.routes.ts
@@ -1,4 +1,8 @@
 export const extensionRoutes = [  {
     path: 'extensions/pdf-templates',
     loadChildren: () => import('./extensions/92aa7eafa53fed03241b9e8420384d94fa519ad8f9643a1003d2e69c3a17d771/routes'),
+  },
+  {
+    path: 'extensions/subscription-feature',
+    loadChildren: () => import('./extensions/subscription-feature-ui/routes'),
   }];
--- src/__admin-ui/src/shared-extensions.module.ts
+++ src/__admin-ui/src/shared-extensions.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import SharedProviders_0_0 from './extensions/92aa7eafa53fed03241b9e8420384d94fa519ad8f9643a1003d2e69c3a17d771/providers';
+import SharedProviders_1_0 from './extensions/subscription-feature-ui/providers';
 
 
 @NgModule({
     imports: [CommonModule, ],
-    providers: [...SharedProviders_0_0],
+    providers: [...SharedProviders_0_0, ...SharedProviders_1_0],
 })
 export class SharedExtensionsModule {}
